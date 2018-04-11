ff00a776e85ac7c12858e0e2102ab5d08f41a3ab
vamsidamisetti <vamsik586@gmail.com>
2018-04-11 17:37:19 +0530

    Add files via upload

---
+++ project.c
@@ -0,0 +1,185 @@
+#include<stdio.h>
+#include<pthread.h>
+#include<sys/types.h>
+#include<unistd.h>
+#include<semaphore.h>
+
+int c=0;
+sem_t x;
+struct sharedtable
+{
+int pen;
+
+int paper;
+
+int qp;
+
+};
+
+void *S1(void *p)
+{
+sem_wait(&x);
+struct sharedtable *s = (struct sharedtable*)p;
+s->pen+=1;
+if(s->pen==0||s->paper==0||s->qp==0)
+{
+	s->pen-=1;
+ sem_post(&x);
+
+wait();
+}
+else
+
+{
+printf("1st student completed the task\n");
+
+c++;
+s->pen=0;
+s->qp=0;
+s->paper=0;
+
+}
+
+sem_post(&x);
+}
+
+void *S2(void *p)
+{ 
+sem_wait(&x);
+
+struct sharedtable *s = (struct sharedtable*)p;
+s->paper+=1;
+if(s->pen==0||s->paper==0||s->qp==0) 
+{	s->paper-=1;
+	sem_post(&x);
+	
+	wait();
+}
+else
+{
+printf("2nd student completed the task\n");
+
+c++;
+s->qp=0;
+s->paper=0;
+s->pen=0;
+
+}
+
+sem_post(&x);
+}
+
+void *S3(void *p)
+{
+sem_wait(&x);
+
+struct sharedtable *s = (struct sharedtable*)p;
+s->qp+=1;
+if(s->pen==0||s->paper==0||s->qp==0)
+{	s->qp-=1;
+	sem_post(&x);
+	wait();
+}
+else
+{
+printf("3rd student completed the task\n");
+c++;
+s->qp=0;
+s->pen=0;
+s->paper=0;
+}
+
+sem_post(&x);
+
+}
+
+int main()
+
+{
+
+sem_init(&x,0,2);
+int a;
+
+pthread_t t1,t2,t3;
+
+struct sharedtable s;
+
+s.pen=0;
+
+s.paper=0;
+
+s.qp=0;
+
+//pthread_create(&t1,NULL,S1,&s);
+
+//pthread_create(&t2,NULL,S2,&s);
+
+//pthread_create(&t3,NULL,S3,&s);
+
+while(c!=3)
+
+{
+
+printf("Enter the things you want to place in the shared table\n")
+;
+printf("Enter 1 for Pen and paper\n");
+
+printf("Enter 2 for pen and qustion paper\n");
+
+printf("Enter 3 for paper and question paper\n");
+
+scanf("%d",&a);
+
+switch(a)
+
+{
+
+case 1:
+
+{
+
+s.pen=1;
+s.paper=1;
+break;
+
+}
+
+case 2:
+
+{
+
+s.pen=1;
+s.qp=1;
+break;
+
+}
+
+
+case 3:
+
+{
+
+s.paper=1;
+s.qp=1;
+break;
+
+}
+default:
+exit(0);
+
+}
+pthread_create(&t1,NULL,S1,&s);
+pthread_create(&t2,NULL,S2,&s);
+pthread_create(&t3,NULL,S3,&s);
+pthread_join(t1,NULL);
+
+pthread_join(t2,NULL);
+
+pthread_join(t3,NULL);
+
+
+}
+
+printf("All students completed the task!!!");
+return 0;
+}
